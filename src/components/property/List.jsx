855347709ccd13739ab62f2f96baf600a3f90d02
Eshwar <eshwaramahendra0120@gmail.com>
2023-06-13 20:17:38 +0530

    added complete property listing 	modified:   public/index.html 	modified:   src/components/SideNav.jsx 	modified:   src/components/TopNav.jsx 	modified:   src/components/property/List.jsx 	new file:   src/components/property/PropertyData.jsx 	modified:   src/context/UserContext.js 	modified:   src/styles/list.css 	modified:   src/styles/sidenav.css 	modified:   src/styles/topnav.css 	modified:   src/styles/utils.css 	modified:   src/utils/EyeLogo.jsx 	modified:   src/utils/SearchLogo.jsx

--- src/components/property/List.jsx
+++ src/components/property/List.jsx
@@ -1,29 +1,90 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import ListGroup from 'react-bootstrap/ListGroup';
 import useFetchGet from '../../utils/useFetch';
+import '../../styles/list.css';
+import SearchLogo from '../../utils/SearchLogo';
+import ImageComp from './PropertyData';
+import PropertyData from './PropertyData';
 
 export default function List() {
 
   const { loginStatus } = useContext(UserContext);
-    const navigate = useNavigate()
-    useEffect(() =>{
-        loginStatus ? navigate('/property') : navigate('/login')
-    },[]);
-    const data = useFetchGet({})
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    loginStatus ? navigate('/property') : navigate('/login')
+  }, []);
+
+  const { data, updateDataRefresh } = useContext(UserContext);
+  const [search, setSearch] = useState(true);
+  const [searchInput, setSearchInput] = useState("");
 
 
-    return (
-      <>
-        {/* {['sm', 'md', 'lg', 'xl', 'xxl'].map((breakpoint) => (
-          <ListGroup key={breakpoint} horizontal={breakpoint} className="">
-            <ListGroup.Item>This ListGroup</ListGroup.Item>
-            <ListGroup.Item>renders horizontally</ListGroup.Item>
-            <ListGroup.Item>on {breakpoint}</ListGroup.Item>
-            <ListGroup.Item>and above!</ListGroup.Item>
-          </ListGroup>
-        ))} */}
-      </>
-    );
+  return (
+    <div className='content-container'>
+      <div className='top-container'>
+        <div className='search-container'>
+          <input
+            type="text"
+            placeholder='Search PPD ID'
+            className='search-bar'
+            onChange={(e) => {
+              setSearchInput(e.target.value);
+            }}
+            value={searchInput} />
+          <div 
+          className='search-logo'
+          onClick={()=>{
+            // setSearch(true)
+          }}
+          >
+            <SearchLogo />
+          </div>
+        </div>
+        <button className='add-btn white-clr'
+          onClick={() => {
+            navigate('/basicinfo')
+          }}
+        >Add Property</button>
+      </div>
+      <div className='data-container'>
+        <table>
+          <thead className='blue-clr-light2'>
+            <tr>
+              <th>PPD ID</th>
+              <th>Image</th>
+              <th>Property</th>
+              <th>Contact</th>
+              <th>Area</th>
+              <th>Views</th>
+              <th>Status</th>
+              <th>Days Left</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              search 
+              ?
+              data.map((item)=>{
+                  if(item._id.toString().includes(searchInput)){
+                    return (<PropertyData item={item} key={item._id}/>)
+                  }else{
+                    return <></>
+                  }
+              })
+              :
+              data.map((item) => {
+                return (
+                  <PropertyData item={item} key={item._id}/>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 
@@ -6,12 +6,97 @@ const UserContext = createContext({});
 
 const UserContextProvider = ({ children }) => {
 
-    const [login , setLogin] = useState(true);
+    const [login, setLogin] = useState(true);
+    // console.log(login)
+    const [data, setData] = useState([
+        {
+            _id: 12345,
+            image: '',
+            property: 'house',
+            contact: "1234567890",
+            area: 'hyderabad',
+            views: 57,
+            status: 'sold',
+            daysLeft: 45,
+        },
+        {
+            _id: 67890,
+            image: '',
+            property: 'house',
+            contact: "1234567890",
+            area: 'hyderabad',
+            views: 57,
+            status: 'sold',
+            daysLeft: 45,
+        },
+        {
+            _id: 54321,
+            image: '',
+            property: 'house',
+            contact: "1234567890",
+            area: 'hyderabad',
+            views: 57,
+            status: 'sold',
+            daysLeft: 45,
+        },
+        {
+            _id: 123450,
+            image: '',
+            property: 'house',
+            contact: "1234567890",
+            area: 'hyderabad',
+            views: 57,
+            status: 'sold',
+            daysLeft: 45,
+        },
+        {
+            _id: 678900,
+            image: '',
+            property: 'house',
+            contact: "1234567890",
+            area: 'hyderabad',
+            views: 57,
+            status: 'sold',
+            daysLeft: 45,
+        },
+        {
+            _id: 543210,
+            image: '',
+            property: 'house',
+            contact: "1234567890",
+            area: 'hyderabad',
+            views: 57,
+            status: 'sold',
+            daysLeft: 45,
+        },
+    ]);
+    const [dataRefresh, setDataRefresh] = useState(true);
+    useEffect(() => {
+        fetch(
+            `http://localhost:8001//api/alldata`
+        )
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                }
+                throw new Error(response.json());
+            })
+            .then(res => {
+                setData(res)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, [])
 
     const value = {
-        loginStatus : login,
-        updateLoginStatus : (boolean)=>{
+        loginStatus: login,
+        updateLoginStatus: (boolean) => {
             setLogin(boolean);
+        },
+        data: data,
+        updateDataRefresh: () => {
+            setDataRefresh(!dataRefresh)
         }
     };
 
@@ -20,7 +105,7 @@ const UserContextProvider = ({ children }) => {
     </UserContext.Provider>
 };
 
-export{
+export {
     UserContext,
     UserContextProvider
 }
