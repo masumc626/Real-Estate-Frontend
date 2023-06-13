c5d147f0686416e9caceec339d8a134fffe9b2dd
Eshwar <eshwaramahendra0120@gmail.com>
2023-06-13 11:53:54 +0530

    added sidenav and routes 	modified:   package-lock.json 	modified:   package.json 	new file:   public/brandLogo.svg 	deleted:    public/favicon.ico 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/Home.jsx 	new file:   src/components/SideNav.jsx 	new file:   src/components/TopNav.jsx 	new file:   src/components/property/List.jsx 	new file:   src/components/user/Login.jsx 	new file:   src/components/user/Signup.jsx 	new file:   src/context/UserContext.js 	modified:   src/index.js 	new file:   src/styles/list.css 	new file:   src/styles/login.css 	new file:   src/styles/sidenav.css 	new file:   src/styles/topnav.css 	new file:   src/styles/utils.css 	new file:   src/utils/AssistanceLogo.jsx 	new file:   src/utils/BrandLogo.jsx 	new file:   src/utils/DropDownLogo copy.jsx 	new file:   src/utils/DropDownLogo.jsx 	new file:   src/utils/EditLogo.jsx 	new file:   src/utils/EyeLogo.jsx 	new file:   src/utils/ImagesLogo.jsx 	new file:   src/utils/ProfileLogo.jsx 	new file:   src/utils/PropertyLogo.jsx 	new file:   src/utils/PropertyViews.jsx 	new file:   src/utils/ReceivedIntrestLogo.jsx 	new file:   src/utils/SearchLogo.jsx 	new file:   src/utils/SentIntrestLogo.jsx 	new file:   src/utils/TariffPlan.jsx 	new file:   src/utils/useFetch.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react';
+// import './App.css';
+import './styles/utils.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/user/Login';
+import SideNav from './components/SideNav';
+import TopNav from './components/TopNav';
+import Signup from './components/user/Signup';
+import List from './components/property/List';
+// import BasicInfo from './components/property/BasicInfo';
+// import Details from './components/property/Details';
+// import GeneralInfo from './components/property/GeneralInfo';
+// import LocationInfo from './components/property/LocationInfo';
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext';
+import Home from './components/Home';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 function App() {
+  const {loginStatus} = useContext(UserContext);
+  console.log(loginStatus)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      {
+        loginStatus ?
+        <>
+        <SideNav/>
+        <TopNav/>
+        </>
+        :
+        <></>
+      }
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/property' element={<List/>}/>
+        {/* <Route path='/basicinfo' element={<BasicInfo/>}/>
+        <Route path='/details' element={<Details/>}/>
+        <Route path='/generalinfo' element={<GeneralInfo/>}/>
+        <Route path='/locationinfo' element={<LocationInfo/>}/> */}
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
